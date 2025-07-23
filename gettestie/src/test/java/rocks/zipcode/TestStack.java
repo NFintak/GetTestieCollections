7701d59d52bd295c53ac8da48b2d2147934fd86d
NFintak <natefintak@gmail.com>
2025-07-23 16:36:01 -0400

    checkpoint

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,6 +1,7 @@
 package rocks.zipcode;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 import java.util.Stack;
 
 public class TestStack {
@@ -13,7 +14,7 @@ public class TestStack {
     public void tearDown() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void TestStack1() {
         Stack<String> stack = new Stack<>();
         stack.push("Hello world");
@@ -21,6 +22,21 @@ public class TestStack {
     }
 
     //add additional test for push?
-
     //add tests for pop, peek, isEmpty
+    @Test
+    public void TestStack2() {
+        Stack<String> stack = new Stack<>();
+        stack.push("test1");
+        stack.push("test2");
+        assertEquals("test2", stack.peek());
+    }
+
+    @Test
+    public void testStack3() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(3);
+        Integer expected = 3;
+        assertEquals(expected, stack.pop());
+    }
 }
