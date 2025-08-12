59bb5860b6a2f4846be1fe2be63d20fd7ac59151
NFintak <natefintak@gmail.com>
2025-08-12 15:59:30 -0400

    checkpoint

--- gettestie/src/test/java/rocks/zipcode/TestHashSet.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashSet.java
@@ -3,9 +3,19 @@ package rocks.zipcode;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.HashSet;
+import rocks.zipcode.Person;
 
 public class TestHashSet {
 
+    public void createTestData() {
+        Person person1 = new Person("person1", 1990);
+        Person person2 = new Person("person2", 2004);
+    }
+
     //add tests for add, clear, contains, isEmpty, and size
+    @Test
+    public void testHashSet1() {
+
+    }
 
 }
